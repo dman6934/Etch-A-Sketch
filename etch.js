2bc820fc12af494fdb9d6407995e70111470cd2c
dman6934 <83676060+dman6934@users.noreply.github.com>
2021-08-20 23:59:32 -0500

    made the box show up and added css
    made the box show up and added css

--- etch.js
+++ etch.js
@@ -1,13 +1,30 @@
-const container_div = document.getElementById("container");
-
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
-      cell.innerText = (c + 1);
+      cell.innerText = "";
       container.appendChild(cell).className = "grid-item";
     };
   };
+
+  let size = 0;
+  const start = document.getElementById('start');
+  start.addEventListener('click', function(){
+    size = prompt('Please enter the side of the grid(max 64):');
+      if(size <= 64){
+        container.innerHTML = '';
+        makeRows(size, size);
+      } else if (size > 64) {
+        alert('You exceeded the limits. Please Try Again:');
+      } 
+  });
+
+  const reset = document.getElementById("reset");
+  reset.addEventListener('click', function () {
+    container.innerHTML = "";
+    makeRows();
+  });
+
+ 
   
-  makeRows(16, 16);
